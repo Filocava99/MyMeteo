9326f6b0a2949fbc47569205385bf3bc83267e0a
Filippo Cavallari <filippo.cavallari99@gmail.com>
2020-12-15 15:07:53 +0100

    Prepared test for the forecast get

--- lib/OWMClient.dart
+++ lib/OWMClient.dart
@@ -5,13 +5,15 @@ import 'package:http/http.dart';
 import 'package:my_meteo/HttpClient.dart';
 import 'package:my_meteo/Weather.dart';
 
-class OWMClient extends HttpClient {
-  final openWeatherMapUrl = "api.openweathermap.org";
-  final String forecastPath = "/data/2.5/forecast";
-  final String weatherPath = "/data/2.5/weather";
-  final String apiKey;
+import 'Forecast.dart';
 
-  OWMClient(this.apiKey);
+class OWMClient extends HttpClient {
+  final _openWeatherMapUrl = "api.openweathermap.org";
+  final String _forecastPath = "/data/2.5/forecast";
+  final String _weatherPath = "/data/2.5/weather";
+  final String _apiKey;
+
+  OWMClient(this._apiKey);
 
   Future<Weather> getWeather(String city, {String stateCode = "", String countryCode = ""}) async {
     Map<String, String> header = HashMap();
@@ -19,12 +21,23 @@ class OWMClient extends HttpClient {
     if (countryCode != "") countryCode = "," + countryCode;
     header["q"] = city + stateCode + countryCode;
     header["appid"] = apiKey;
-    var uri = Uri.http(openWeatherMapUrl, weatherPath, header);
+    var uri = Uri.http(_openWeatherMapUrl, _weatherPath, header);
     Response response = await get(uri, headers: header);
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load weather');
     }
+  }
+
+  Future<Forecast> getForecast(String city, {String stateCode = "", String countryCode = ""}) async{
+    Map<String, String> header = HashMap();
+    if (stateCode != "") stateCode = "," + stateCode;
+    if (countryCode != "") countryCode = "," + countryCode;
+    header["q"] = city + stateCode + countryCode;
+    header["appid"] = apiKey;
+    var uri = Uri.http(_openWeatherMapUrl, _forecastPath, header);
+    Response response = await get(uri, headers: header);
+    print(jsonDecode(response.body));
   }
 }
--- test/OWMClientTest.dart
+++ test/OWMClientTest.dart
@@ -5,9 +5,14 @@ import 'package:my_meteo/Weather.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('GET test. Http response should be 200', () async {
+  test('Weather test. The city name should be \'Provincia di Ravenna\'', () async {
     OWMClient owmClient = OWMClient("621a656f252b4f5eb71989a3cb634ae4");
     Weather weather = await owmClient.getWeather("Ravenna");
     expect(weather.name, "Provincia di Ravenna");
+  });
+
+  test('Forecast test. ', () async {
+    OWMClient owmClient = OWMClient("621a656f252b4f5eb71989a3cb634ae4");
+    await owmClient.getForecast("Ravenna");
   });
 }
